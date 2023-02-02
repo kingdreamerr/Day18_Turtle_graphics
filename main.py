0040075060277f0df928cc0e8dab0e0c35355849
kingdreamerr <jackmvula458@gmail.com>
2023-02-03 00:38:56 +0200

    Create cat object and set starting values

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import turtle as t_module
 from turtle import Screen
 t_module.colormode(255)
 from random import choice
+
+cat = t_module.Turtle()
+cat.hideturtle()
+cat.speed("fastest")
+number_of_dots =100
